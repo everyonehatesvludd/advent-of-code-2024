6956b9c7b03b06f933f9136b3e8dca00745f4ad4
slezkipotekli <vreginalld@gmail.com>
2024-12-02 11:50:15 +0300

    Day 1 code optimization. Interim results will now be stored in memory instead of separate file. Optimized input file path obtaining. Day 2 directory and input data added.

--- src/Day1/Day1a.java
+++ src/Day1/Day1a.java
@@ -1,7 +1,6 @@
 package Day1;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.*;
@@ -11,41 +10,34 @@ import java.util.*;
  */
 public class Day1a {
     public static void main(String[] args) throws IOException {
-        String currentPath = Path.of("").toAbsolutePath().resolve("src/Day1").toString();
-        File input = new File(String.format("%s\\input.txt", currentPath));
-        File output = new File(String.format("%s\\distances.txt", currentPath));
-        output.createNewFile();
+        try (Scanner scanner = new Scanner(new File(Path.of("src/Day1/input.txt").toString()))) {
+            List<Integer> row1 = new ArrayList<>();
+            List<Integer> row2 = new ArrayList<>();
+            List<Integer> distances = new ArrayList<>();
 
-        FileWriter writer = new FileWriter(String.format("%s\\distances.txt", currentPath));
-        Scanner scanner1 = new Scanner(input);
+            while (scanner.hasNextLine()){
+                String[] locations = scanner.nextLine().split(" {3}");
 
-        List<Integer> row1 = new ArrayList<>();
-        List<Integer> row2 = new ArrayList<>();
+                row1.add(Integer.parseInt(locations[0]));
+                row2.add(Integer.parseInt(locations[1]));
+            }
+            scanner.close();
 
-        while (scanner1.hasNextLine()){
-            String[] locations = scanner1.nextLine().split(" {3}");
+            Collections.sort(row1);
+            Collections.sort(row2);
 
-            row1.add(Integer.parseInt(locations[0]));
-            row2.add(Integer.parseInt(locations[1]));
+            int distance;
+            for (int i = 0; i < row1.size(); i++) {
+                distance = row1.get(i) - row2.get(i);
+                distances.add(Math.abs(distance));
+            }
+
+            int result = 0;
+            for (Integer integer : distances) {
+                result += integer;
+            }
+
+            System.out.println(result);
         }
-        scanner1.close();
-
-        Collections.sort(row1);
-        Collections.sort(row2);
-
-        int distance;
-        for (int i = 0; i < row1.size(); i++) {
-            distance = row1.get(i) - row2.get(i);
-            writer.write(Math.abs(distance)+"\n");
-        }
-        writer.close();
-
-        Scanner scanner2 = new Scanner(output);
-        int result = 0;
-        while (scanner2.hasNextInt()){
-            result += scanner2.nextInt();
-        }
-        System.out.println(result);
-        scanner2.close();
     }
 }
--- src/Day1/Day1b.java
+++ src/Day1/Day1b.java
@@ -1,7 +1,5 @@
 package Day1;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -13,46 +11,34 @@ import java.util.Scanner;
  */
 public class Day1b {
     public static void main(String[] args) throws IOException {
-        String currentPath = Path.of("").toAbsolutePath().resolve("src/Day1").toString();
-        File input = new File(String.format("%s\\input.txt", currentPath));
+        try (Scanner scanner = new Scanner(Path.of("src/Day1/input.txt"))){
+            List<Integer> row1 = new ArrayList<>();
+            List<Integer> row2 = new ArrayList<>();
+            List<Long> similarity_scores = new ArrayList<>();
 
-        File output = new File(String.format("%s\\similarity_scores.txt", currentPath));
-        boolean outputCreation = output.createNewFile();
-        if (!outputCreation)
-            System.out.println("Unable to create file");
+            while (scanner.hasNextLine()){
+                String[] locations = scanner.nextLine().split(" {3}");
 
-        FileWriter writer = new FileWriter(String.format("%s\\similarity_scores.txt", currentPath));
-        Scanner inputScanner = new Scanner(input);
+                row1.add(Integer.parseInt(locations[0]));
+                row2.add(Integer.parseInt(locations[1]));
+            }
+            scanner.close();
 
-        List<Integer> row1 = new ArrayList<>();
-        List<Integer> row2 = new ArrayList<>();
+            for (int i : row1){
+                long duplicates = row2
+                        .stream()
+                        .filter(n -> n == i)
+                        .count();
 
-        while (inputScanner.hasNextLine()){
-            String[] locations = inputScanner.nextLine().split(" {3}");
+                long score = i * duplicates;
+                similarity_scores.add(score);
+            }
 
-            row1.add(Integer.parseInt(locations[0]));
-            row2.add(Integer.parseInt(locations[1]));
+            long result = 0;
+            for (long i : similarity_scores)
+                result += i;
+            System.out.println(result);
         }
-        inputScanner.close();
-
-        for (int i : row1){
-            long duplicates = row2
-                    .stream()
-                    .filter(n -> n == i)
-                    .count();
-
-            long score = i * duplicates;
-            writer.write(score+"\n");
-        }
-        writer.close();
-
-        Scanner outputScanner = new Scanner(output);
-        int result = 0;
-        while (outputScanner.hasNextInt())
-            result += outputScanner.nextInt();
-
-        System.out.println(result);
-        outputScanner.close();
 
     }
 }
